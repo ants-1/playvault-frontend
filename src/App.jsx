534d86cbfa30272b64553353e57efab0e60441f3
Anthony <anthonysalisbury98@outlook.com>
2025-08-04 00:24:55 +0100

    Setup Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { Link } from 'react-router'
-import './App.css'
+import { Link } from "react-router";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Link to="/login">Login</Link>
       <Link to="/sign-up">Sign Up</Link>
+      <h1 class="text-3xl text-blue-400 font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
